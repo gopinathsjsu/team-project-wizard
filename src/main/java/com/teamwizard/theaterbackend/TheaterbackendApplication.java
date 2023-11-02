952632f65d3c855386b6bd711cf16bb822672b16
varoOP <46842159+varoOP@users.noreply.github.com>
2023-11-01 22:50:32 -0700

    Init backend

---
+++ src/main/java/com/teamwizard/theaterbackend/TheaterbackendApplication.java
@@ -0,0 +1,13 @@
+package com.teamwizard.theaterbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheaterbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheaterbackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/teamwizard/theaterbackend/TheaterbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teamwizard.theaterbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheaterbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
