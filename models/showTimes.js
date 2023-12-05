91a0c1ee809f67919476195ab681d00a7489d958
Varshith Pabbisetty <krishnavarshith.pabbisetty@sjsu.edu>
2023-12-04 22:21:37 -0800

    Implement showtimes model and API.

---
+++ models/showTimes.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+
+const showTimeSchema = new mongoose.Schema({
+    // showTimeId: {
+    //     type: String,
+    // },
+    movieId: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+    },
+    screenId: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+    },
+    startTime: { 
+        type: Date,
+    },
+    endTime: { 
+        type: Date,
+    },
+    price: { 
+        type: Number
+    },
+    discountPrice: { 
+        type: Number,
+    },
+    seatsBooked: {
+        type: String,
+    },
+    isActive: {
+        type: Boolean,
+        default: true
+    }
+}, {
+    timestamps: true
+});
+
+
+const ShowTime = mongoose.model('showTimes', showTimeSchema);
+
+
+module.exports = ShowTime;
