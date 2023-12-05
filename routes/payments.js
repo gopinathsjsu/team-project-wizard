d3d825a3f4b14142c7eeba9c478f645cd94f8cdc
Varshith Pabbisetty <krishnavarshith.pabbisetty@sjsu.edu>
2023-12-04 23:28:00 -0800

    Update in payments API

--- routes/payments.js
+++ routes/payments.js
@@ -24,38 +24,38 @@ router.post('/', async (req, res) => {
     }
 });
 
-// router.get('/:transactionId', async (req, res) => {
-//     try {
-//         const transactionId = req.params.transactionId;
-//         const payment = await Payment.findOne({ transactionId: transactionId });
+router.get('/:transactionId', async (req, res) => {
+    try {
+        const transactionId = req.params.transactionId;
+        const payment = await Payment.findOne({ transactionId: transactionId });
 
-//         if (!payment) {
-//             return res.status(404).send('Payment not found');
-//         }
+        if (!payment) {
+            return res.status(404).send('Payment not found');
+        }
 
-//         res.json(payment);
-//     } catch (error) {
-//         console.error('Error in fetching payment:', error);
-//         res.status(500).send('Internal Server Error');
-//     }
-// });
+        res.json(payment);
+    } catch (error) {
+        console.error('Error in fetching payment:', error);
+        res.status(500).send('Internal Server Error');
+    }
+});
 
-// router.put('/:transactionId', async (req, res) => {
-//     try {
-//         const transactionId = req.params.transactionId;
-//         const { status } = req.body;
+router.put('/:transactionId', async (req, res) => {
+    try {
+        const transactionId = req.params.transactionId;
+        const { status } = req.body;
 
-//         const updatedPayment = await Payment.findOneAndUpdate(transactionId, { status }, { new: true });
+        const updatedPayment = await Payment.findOneAndUpdate(transactionId, { status }, { new: true });
 
-//         if (!updatedPayment) {
-//             return res.status(404).send('Payment not found');
-//         }
+        if (!updatedPayment) {
+            return res.status(404).send('Payment not found');
+        }
 
-//         res.json({ message: "Payment updated successfully", status: HTTP_STATUS_CODES.OK, payment: updatedPayment });
-//     } catch (error) {
-//         console.error('Error in updating payment:', error);
-//         res.status(500).send('Internal Server Error');
-//     }
-// });
+        res.json({ message: "Payment updated successfully", status: HTTP_STATUS_CODES.OK, payment: updatedPayment });
+    } catch (error) {
+        console.error('Error in updating payment:', error);
+        res.status(500).send('Internal Server Error');
+    }
+});
 
 module.exports = router;
