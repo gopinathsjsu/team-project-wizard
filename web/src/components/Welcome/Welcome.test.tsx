de7ad38714c564386d4cfd8519b35cdd71b37bff
Varshith Pabbisetty <krishnavarshith.pabbisetty@sjsu.edu>
2023-12-04 22:08:46 -0800

    Deleted previous folder code

--- web/src/components/Welcome/Welcome.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render } from '@test-utils';
-import { Welcome } from './Welcome';
-import '@testing-library/jest-dom/extend-expect';
-
-describe('Welcome component', () => {
-  it('has correct Vite guide link', () => {
-    render(<Welcome />);
-  });
-});
