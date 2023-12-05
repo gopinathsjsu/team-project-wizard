04fb2f19c190cf632aa51d7ecde839f7abb973fc
Varshith Pabbisetty <krishnavarshith.pabbisetty@sjsu.edu>
2023-12-04 22:25:04 -0800

    Create booking model and booking API.

---
+++ models/tickets.js
@@ -0,0 +1,56 @@
+const mongoose = require('mongoose');
+
+
+const ticketSchema = new mongoose.Schema({
+    // id: { 
+    //     type: String, 
+    //     required: true 
+    // },
+    userId: {
+        type: String,
+        required: true
+    },
+    movieId: {
+        type: String,
+        required: true
+    },
+    screenId: {
+        type: String,
+        required: true
+    },
+    transactionId: {
+        type: String,
+        required: true
+    },
+    showTime: {
+        type: Date,
+        default: Date.now
+    },
+    seats: {
+        type: String,
+        required: true
+    },
+    numberOfSeats: {
+        type: Number,
+        required:true
+    },
+    qrUrls: {
+        type: [String], // Change the type to string
+        required: true
+    },
+    totalCost:{
+        type: Number,
+        required: true
+    },
+    isActive: {
+        type: Boolean,
+        default: true
+    }
+}, {
+    timestamps: true,
+});
+
+
+const TicketModel = mongoose.model('tickets', ticketSchema);
+
+module.exports = TicketModel;
