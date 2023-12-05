8429be68ccff8ef9b30468bc1297801e7a036bc0
Varshith Pabbisetty <krishnavarshith.pabbisetty@sjsu.edu>
2023-12-04 22:18:10 -0800

    Develop movie model and movie listing API.

---
+++ models/movies.js
@@ -0,0 +1,66 @@
+const mongoose = require('mongoose');
+
+
+const movieSchema = new mongoose.Schema({
+  // movieId: { 
+  //   type: String
+  // },
+  title: {
+    type: String
+  },
+  cast: {
+    type: Array
+  },
+  crew: { 
+    type: Array 
+  },
+  description: { 
+    type: String 
+  },
+  format: {
+    type:String
+  },
+  genres: { 
+    type: Array 
+  },
+  languages: {
+    type:Array
+  },
+  runTime:{
+    type: Number
+  },
+  rating: { 
+    type: Number 
+  },
+  movieTrailerUrl: {
+    type: String
+  },
+  releaseDate:{
+    type: Date
+  },
+  ticketPrice: {
+    type: Number
+  },
+  ticketsSold: {
+    type: Number
+  },
+  posterUrl: { 
+    type: String 
+  },
+  certificate: {
+    type: String
+  },
+  popularity: {
+    type: Number
+  },
+  isActive: {
+    type: Boolean
+  },
+}, {
+  timestamps: true,
+});
+
+
+const Movie = mongoose.model('movie', movieSchema);
+
+module.exports = Movie;
