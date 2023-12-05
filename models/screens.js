e282f47b34d342f18c7fdd2075fdf1464066ecf3
Varshith Pabbisetty <krishnavarshith.pabbisetty@sjsu.edu>
2023-12-04 22:19:12 -0800

    Set up theater and screen models.

---
+++ models/screens.js
@@ -0,0 +1,63 @@
+const mongoose = require('mongoose');
+
+const status = {
+    AVAILABLE: 0,
+    BOOKED: 1,
+    BLOCKED: 2,
+}
+
+const screenSchema = new mongoose.Schema({
+    // screenId: {
+    //     type: String,
+    // },
+    screenType: {
+        type: String,
+        required: true
+    },
+    seatingCapacity: {
+        type: Number,
+        required: true
+    },
+    screenName: {
+        type: String,
+        required: true
+    },
+    rows: {
+        type: Number,
+        required: true
+    },
+    columns: {
+        type: Number,
+        required: true
+    },
+    cost: {
+        type: Number
+    },
+    theatreId: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+    },
+    seatsAvailable: {
+        type: Number,
+        required: true
+    },
+    seats: {
+        type: String,
+        required: true
+    },
+    occupancyStatus: {
+        type: [String],
+    },
+    isActive: {
+        type: Boolean,
+        default: true
+    }
+}, {
+    timestamps: true
+});
+
+
+const Screen = mongoose.model('screens', screenSchema);
+
+
+module.exports = Screen;
